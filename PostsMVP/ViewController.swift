152d39fccee89bb3b066c5d06e5b0c7468a97190
danyaqq <fedak220@gmail.com>
2022-04-09 00:12:37 +0300

    Initial Commit

---
+++ PostsMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsMVP
+//
+//  Created by Даня on 09.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
